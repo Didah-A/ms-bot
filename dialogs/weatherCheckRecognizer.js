98c77687ac391c52c65d20d92d2072a871e888bc
Didacus <didacus.abongo@andela.com>
2020-03-11 14:19:47 +0300

    Add weather bot LUIS recognizer

---
+++ dialogs/weatherCheckRecognizer.js
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { LuisRecognizer } = require('botbuilder-ai');
+
+class WeatherCheckRecognizer {
+    constructor(config) {
+        const luisIsConfigured = config && config.applicationId && config.endpointKey && config.endpoint;
+        if (luisIsConfigured) {
+            this.recognizer = new LuisRecognizer(config, {}, true);
+        }
+    }
+
+    get isConfigured() {
+        return (this.recognizer !== undefined);
+    }
+
+    /**
+     * Returns an object with preformatted LUIS results for the bot's dialogs to consume.
+     * @param {TurnContext} context
+     */
+    async executeLuisQuery(context) {
+        return await this.recognizer.recognize(context);
+    }
+
+    getCityEntity(result) {
+        if (result.entities.$instance.For) return { city: result.entities.$instance.For[0].text };
+    }
+}
+
+module.exports.WeatherCheckRecognizer = WeatherCheckRecognizer;
